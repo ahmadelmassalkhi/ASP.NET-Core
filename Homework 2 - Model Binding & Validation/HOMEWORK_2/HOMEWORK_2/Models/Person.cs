13aed125b193bc773af47410137f4a9ea86c5d12
ahmadelmassalkhi <ahmad.almassalkhi@st.ul.edu.lb>
2024-04-07 22:46:49 +0300

    Delete Homework 2 - Model Binding & Validation directory

--- Homework 2 - Model Binding & Validation/HOMEWORK_2/HOMEWORK_2/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace homework2.Models
-{
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime DateOfBirth { get; set; }
-    }
-}
