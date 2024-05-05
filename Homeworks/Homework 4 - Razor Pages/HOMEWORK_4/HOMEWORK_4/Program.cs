8776ef299bf8a908d7442dbbd1ab870ab05539df
ahmadmassalkhi <massalkhiahmad18@gmail.com>
2024-05-05 23:30:07 +0300

    Add custom error page through `UseStatusCodePagesWithReExecute`

--- Homeworks/Homework 4 - Razor Pages/HOMEWORK_4/HOMEWORK_4/Program.cs
+++ Homeworks/Homework 4 - Razor Pages/HOMEWORK_4/HOMEWORK_4/Program.cs
@@ -11,10 +11,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseStatusCodePagesWithReExecute("/Error");
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
