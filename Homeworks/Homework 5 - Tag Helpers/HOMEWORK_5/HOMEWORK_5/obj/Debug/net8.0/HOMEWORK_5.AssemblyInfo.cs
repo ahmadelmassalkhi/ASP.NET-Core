b4818c99fe7694da7efbc468b835ec89bc320c41
ahmadmassalkhi <massalkhiahmad18@gmail.com>
2024-05-16 10:37:21 +0300

    Run

--- Homeworks/Homework 5 - Tag Helpers/HOMEWORK_5/HOMEWORK_5/obj/Debug/net8.0/HOMEWORK_5.AssemblyInfo.cs
+++ Homeworks/Homework 5 - Tag Helpers/HOMEWORK_5/HOMEWORK_5/obj/Debug/net8.0/HOMEWORK_5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HOMEWORK_5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76362d617a801eb643fd2add048c5a0bf233ba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06dfc3161df4d5be5fc8439dfc13afa05ec9446")]
 [assembly: System.Reflection.AssemblyProductAttribute("HOMEWORK_5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HOMEWORK_5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
