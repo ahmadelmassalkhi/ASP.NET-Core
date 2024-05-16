7a441b39b31759bcc180357a20baf019fcf3a49d
ahmadmassalkhi <massalkhiahmad18@gmail.com>
2024-05-16 16:57:04 +0300

    Add migration & update database

--- Homeworks/Homework 3 - Dependency Injection & Entity Framework Core/HOMEWORK_3/HOMEWORK_3/obj/Debug/net8.0/HOMEWORK_3.AssemblyInfo.cs
+++ Homeworks/Homework 3 - Dependency Injection & Entity Framework Core/HOMEWORK_3/HOMEWORK_3/obj/Debug/net8.0/HOMEWORK_3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HOMEWORK_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4818c99fe7694da7efbc468b835ec89bc320c41")]
 [assembly: System.Reflection.AssemblyProductAttribute("HOMEWORK_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HOMEWORK_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
